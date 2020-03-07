540dc14451d0f4a1da7be32a84555e1a289c25bb
Oscar Smith-Jones <ozzysmithjones@gmail.com>
2020-03-07 00:34:23 +0000

    Revert "Revert "AI weight system""
    This reverts commit be966b69afa94d719960c9be80a29afeb66bcbad.

--- Assets/Scripts/AI/AI.cs
+++ Assets/Scripts/AI/AI.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class AI : Faction
 {
     
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 =======
+=======
+>>>>>>> eef69385ad9dd06bb4d34d8e2ec82af48de9bee0
    public AI(int index, Color flagColor, string factionName) : base(index,flagColor,factionName)
    {
 
    }
+<<<<<<< HEAD
 
     public override void Update(float deltaTime)
     {
@@ -21,4 +25,7 @@ public class AI : Faction
     }
 
 >>>>>>> Stashed changes
+=======
+    
+>>>>>>> eef69385ad9dd06bb4d34d8e2ec82af48de9bee0
 }
